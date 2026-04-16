92ce58088226e793e475d0de9ac2e5c5c711886e
Haonan Ge <143364746+Haonan-Ge@users.noreply.github.com>
2026-04-16 14:59:43 +0800

    Update purgecss.config.js

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,19 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  safelist: {
+    standard: [
+      "misc-section",
+      "misc-prompt",
+      "misc-lead",
+      "misc-gallery-frame",
+      "misc-gallery",
+      "visitor-map",
+      "visitor-map-inner",
+      "section-heading",
+      "section-heading--plain",
+      "heading-icon",
+    ],
+  },
 };
+
